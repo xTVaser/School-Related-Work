2a212ee2db3218e5e7c598ad0d8297b6d95b60b4
Tyler Wilding <xtvaser@gmail.com>
2016-03-11 20:47:53 -0500

    Added comments

--- Analysis of Algorithms/Assignment 4/src/ChangeMaking.java
+++ Analysis of Algorithms/Assignment 4/src/ChangeMaking.java
@@ -1,77 +1,106 @@
-import javafx.scene.control.TextFormatter;
-
-import java.util.Random;
 import java.util.Scanner;
 
 /**
  * Created by Tyler Wilding on 11/03/16.
  * Assignment 4 - COSC
+ * Description - Finds the minimum amount of coins needed to make change given a list of denominations
  */
 public class ChangeMaking {
 
     public static void main(String[] args) {
 
+        //Get the User input
         Scanner in = new Scanner(System.in);
-            System.out.println("Please enter the denominations of coins separated by spaces: ");
-            String[] denominationsString =  in.nextLine().split("\\s"); //{"5", "1", "2", "10", "6", "2"};
-            System.out.println("Please enter the amount you need to make change for: ");
-            int amount = Integer.parseInt(in.nextLine());
+        System.out.println("Please enter the denominations of coins separated by spaces: ");
+        String[] denominationsString =  in.nextLine().split("\\s");
+        System.out.println("Please enter the amount you need to make change for: ");
+        int amount = Integer.parseInt(in.nextLine());
 
-            int[] denominations = new int[denominationsString.length];
-            int[] minCoins = new int[amount+1];
+        //Make the two arrays
+        int[] denominations = new int[denominationsString.length];
+        int[] minCoins = new int[amount+1];
 
-            populateCoinArray(denominationsString, denominations);
+        //Put the user values into the array
+        populateCoinArray(denominationsString, denominations);
 
-            System.out.println("Making change for "+amount+" cents, requires "+changeAlgorithm(denominations, minCoins)+" coins.");
-
+        //Call the algorithm and output the answer
+        System.out.println("Making change for "+amount+" cents, requires "+changeAlgorithm(denominations, minCoins)+" coins.");
     }
 
+    /**
+     * Will place numbers seperated by spaces into an integer array
+     * @param coins String array of numbers
+     * @param array Int array of those numbers
+     */
     public static void populateCoinArray(String[] coins, int[] array) {
 
         for(int i = 0; i < array.length; i++)
             array[i] = Integer.parseInt(coins[i]);
     }
 
+    /**
+     * Will pass through all numbers from 1...n where n is the amount needed for change.
+     * @param coins array of denomination values.
+     * @param minCoins array of the minimum number of coins needed for each amount along the way
+     * @return An integer containing the minimum number of coins needed for the requested amount.
+     */
     public static int changeAlgorithm(int[] coins, int[] minCoins) {
 
+        //First value is 0
         minCoins[0] = 0;
 
-        for(int i = 1; i < minCoins.length; i++) {
+        //Loop through the array to fill every spot
+        for(int i = 1; i < minCoins.length; i++)
+            //Each time we call the method to determine the best coin to choose for each spot.
             minChange(minCoins, coins, i);
 
-
-        }
-        
+        //The answer is in the last position of the array
         return minCoins[minCoins.length-1];
     }
 
+    /**
+     * Will determine the minimum number of coins needed based on previous calculations.
+     * @param minCoins Array that holds all previous calculations on the minum number of coins
+     * @param coins Denomination array
+     * @param value The value we are attempting to solve for at this point.
+     */
     public static void minChange(int[] minCoins, int[] coins, int value) {
 
         int smallestAmount = Integer.MAX_VALUE;
 
         String output = "F["+value+"] = ";
 
+        //Loop through all denominations
         for(int i = 0; i < coins.length; i++) {
 
+            //If the denomination is greater than what we are making change for, skip it.
             if(coins[i] > value)
                 continue;
 
+            //Use the previous calculation to determine this amounts requirement.
             int delta = minCoins[value - coins[i]];
 
             if(i != 0)
                 output += " + ";
-
             output += "min(F["+value+" - "+coins[i]+"])";
 
-
-
+            //Else, if its the largest denomination that will work, set it.
             if(delta < smallestAmount)
                 smallestAmount = delta;
         }
 
-        minCoins[value] = smallestAmount+1;
+        //The algorithm in the book has a flaw where if you do not give it a denomination to work in all cases, for example 1,
+        //Then things will break, this solves some problems but not all of them.
+        if(smallestAmount == Integer.MAX_VALUE)
+            output += " = Impossible given the denominations";
 
-        output += " = "+minCoins[value];
+        else {
+
+            //The answer is the previous calculations total coins, plus 1 because we are adding a coin right now.
+            minCoins[value] = smallestAmount + 1;
+
+            output += " = " + minCoins[value];
+        }
 
         System.out.println(output);
     }
--- Analysis of Algorithms/Assignment 4/src/CoinRow.java
+++ Analysis of Algorithms/Assignment 4/src/CoinRow.java
@@ -10,12 +10,14 @@ public class CoinRow {
 
     public static void main(String[] args) {
 
+        //User Input
         Scanner in = new Scanner(System.in);
 
         System.out.println("Stress Test: (y/n): ");
         if(in.nextLine().equalsIgnoreCase("y"))
             stressTest(1000000);
 
+        //Split the strings and turn them into arrays,call the algorithm.
         else {
 
             System.out.println("Please enter the values of the coins, seperated by spaces: ");
@@ -30,18 +32,35 @@ public class CoinRow {
         }
     }
 
+
+    /**
+     * Method that will take in a string array and convert it to an integer array
+     * @param coins String array of coin values
+     * @param array Int array to hold the coin values
+     */
     public static void populateCoinArray(String[] coins, int[] array) {
 
         for(int i = 0; i < array.length-1; i++)
             array[i+1] = Integer.parseInt(coins[i]);
     }
 
+    /**
+     * Main algorithm procedure, will find the most money that can be picked up without picking up adjacent coins.
+     * @param coins Array of coins to pick up
+     * @param money Array that will keep track of the money we have accumulated
+     * @return The maximum we can pick up.
+     */
     public static int coinRowAlgorithm(int[] coins, int[] money) {
 
+        //First two numbers are static.
         money[0] = 0;
         money[1] = coins[1];
 
+        //Loop through both arrays
         for(int i = 2; i < coins.length; i++) {
+            //Determine whether or not it is better to either:
+            //1. Pick up the current coin, and drop the previous one or.
+            //2. Keep the last 2 coin combinations and skip this one.
             money[i] = Math.max((coins[i] + money[i-2]), money[i-1]);
             System.out.printf("F[%4d] = max(%3d + %3d, %3d) = %3d\n", i, coins[i], money[i-2], money[i-1], money[i]);
         }
@@ -49,6 +68,10 @@ public class CoinRow {
         return money[money.length-1];
     }
 
+    /**
+     * Will generate random coins.
+     * @param iterations Number of coins in the coin row.
+     */
     public static void stressTest(int iterations) {
 
         int[] coins = new int[iterations+1];
@@ -56,7 +79,7 @@ public class CoinRow {
         Random rand = new Random();
 
         for(int i = 0; i < coins.length; i++)
-            coins[i] = rand.nextInt(50);
+            coins[i] = rand.nextInt(50)+1;
 
         int[] money = new int[iterations+1];
 
